778a1aceec62ae127bf34316fd9fe021b37a49a1
jonasrafnkjaer <jonas.rafn.kjaer@gmail.com>
2025-11-24 10:33:17 +0100

    Added sex vs survival analysis (SE, CI, chi-square, pseudo-R2)

--- Jonas_test.R
+++ Jonas_test.R
@@ -1,22 +1,35 @@
 df <- read.csv("data/med_data_tidy.csv")
-head(df)
 
-#finding the mortality and survival rate for each of the sex
-df$sex <- factor(df$sex, levels = c("female", "male"))
 library(dplyr)
-
-total_survival <- df %>%
-  group_by(sex) %>%
-  summarise(
-    n = n(),
-    deaths = sum(death, na.rm = TRUE),
-    mortality_rate = mean(death, na.rm = TRUE),
-    survival_rate = 1 - mortality_rate
-  )
-total_survival
-
-#plotting the total survival by sex
 library(ggplot2)
+
+# write sex in the correcet order.
+df <- df %>%
+  mutate(sex = factor(sex, levels = c("female", "male")))
+
+# subset with cancer-patienter
+df_cancer <- df %>% filter(diag_class == "Cancer")
+
+# sex summary.
+summarise_by_sex <- function(data) {
+  data %>%
+    group_by(sex) %>%
+    summarise(
+      n = n(),
+      death_rate = mean(death, na.rm = TRUE),
+      survival_rate = 1 - death_rate,
+      se = sqrt(death_rate * (1 - death_rate) / n),
+      CI_lower = death_rate - 2 * se,
+      CI_upper = death_rate + 2 * se,
+      .groups = "drop"
+    )
+}
+
+# simple stats.
+total_stats  <- summarise_by_sex(df)
+cancer_stats <- summarise_by_sex(df_cancer)
+
+# we plot the survival rate for total group
 ggplot(df, aes(x = sex, fill = factor(death))) +
   geom_bar(position = "fill") +
   labs(
@@ -25,20 +38,7 @@ ggplot(df, aes(x = sex, fill = factor(death))) +
     title = "Total Survival by Sex"
   )
 
-
-#subset of cancer patients:
-df_cancer <- df %>% filter(diag_class == "Cancer")
-cancer_survival <- df_cancer %>%
-  group_by(sex) %>%
-  summarise(
-    n = n(),
-    deaths = sum(death, na.rm = TRUE),
-    mortality_rate = mean(death, na.rm = TRUE),
-    survival_rate = 1 - mortality_rate
-  )
-cancer_survival
-
-#barplot of the cancer survival
+# we plot the survival rate for cancer sub_group
 ggplot(df_cancer, aes(x = sex, fill = factor(death))) +
   geom_bar(position = "fill") +
   labs(
@@ -47,8 +47,52 @@ ggplot(df_cancer, aes(x = sex, fill = factor(death))) +
     title = "Cancer Survival by Sex"
   )
 
+##perform a chi square test to get p_value.
+chi_total  <- chisq.test(table(df$sex, df$death))
+chi_cancer <- chisq.test(table(df_cancer$sex, df_cancer$death))
 
-#pvalue
-#we can perform a chi square test to see the mortality rate
-chisq.test(table(df$sex, df$death))
-chisq.test(table(df_cancer$sex, df_cancer$death))
+chi_total
+chi_cancer
+
+#logistic regression to get r^2 for both cases.
+model_total  <- glm(death ~ sex, data = df,        family = binomial)
+model_cancer <- glm(death ~ sex, data = df_cancer, family = binomial)
+
+pseudoR2_total  <- 1 - model_total$deviance  / model_total$null.deviance
+pseudoR2_cancer <- 1 - model_cancer$deviance / model_cancer$null.deviance
+
+pseudoR2_total
+pseudoR2_cancer
+
+# Table to format results nicely.
+combined_table <- tibble::tibble(
+  Analysis        = c("All patients", "Cancer patients"),
+  Chi_square_p    = c(chi_total$p.value, chi_cancer$p.value),
+  Pseudo_R2       = c(pseudoR2_total,   pseudoR2_cancer),
+  Male_death_rate = c(
+    total_stats$death_rate[total_stats$sex == "male"],
+    cancer_stats$death_rate[cancer_stats$sex == "male"]
+  ),
+  Female_death_rate = c(
+    total_stats$death_rate[total_stats$sex == "female"],
+    cancer_stats$death_rate[cancer_stats$sex == "female"]
+  ),
+  SE_male = c(
+    total_stats$se[total_stats$sex == "male"],
+    cancer_stats$se[cancer_stats$sex == "male"]
+  ),
+  SE_female = c(
+    total_stats$se[total_stats$sex == "female"],
+    cancer_stats$se[cancer_stats$sex == "female"]
+  ),
+  N_male = c(
+    total_stats$n[total_stats$sex == "male"],
+    cancer_stats$n[cancer_stats$sex == "male"]
+  ),
+  N_female = c(
+    total_stats$n[total_stats$sex == "female"],
+    cancer_stats$n[cancer_stats$sex == "female"]
+  )
+)
+
+combined_table
