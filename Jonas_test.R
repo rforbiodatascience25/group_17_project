2e27749179cb4f198799bf5a9a1be36e8f13d3eb
jonasrafnkjaer <jonas.rafn.kjaer@gmail.com>
2025-11-25 09:21:31 +0100

    changed format to qurto and added sex vs survival table for further analysis.

--- Jonas_test.R
+++ Jonas_test.R
@@ -2,18 +2,20 @@ df <- read.csv("data/med_data_tidy.csv")
 
 library(dplyr)
 library(ggplot2)
-
-# write sex in the correcet order.
-df <- df %>%
+library(tibble)
+library(here)
+# write sex in the correct order.
+df <- df |>
   mutate(sex = factor(sex, levels = c("female", "male")))
 
 # subset with cancer-patienter
-df_cancer <- df %>% filter(diag_class == "Cancer")
+df_cancer <- df |> filter(diag_class == "Cancer")
 
+df
 # sex summary.
 summarise_by_sex <- function(data) {
-  data %>%
-    group_by(sex) %>%
+  data |>
+    group_by(sex) |>
     summarise(
       n = n(),
       death_rate = mean(death, na.rm = TRUE),
@@ -28,6 +30,8 @@ summarise_by_sex <- function(data) {
 # simple stats.
 total_stats  <- summarise_by_sex(df)
 cancer_stats <- summarise_by_sex(df_cancer)
+total_stats
+cancer_stats
 
 # we plot the survival rate for total group
 ggplot(df, aes(x = sex, fill = factor(death))) +
@@ -47,28 +51,28 @@ ggplot(df_cancer, aes(x = sex, fill = factor(death))) +
     title = "Cancer Survival by Sex"
   )
 
-##perform a chi square test to get p_value.
+## perform a chi square test to get p_value.
 chi_total  <- chisq.test(table(df$sex, df$death))
 chi_cancer <- chisq.test(table(df_cancer$sex, df_cancer$death))
 
 chi_total
 chi_cancer
 
-#logistic regression to get r^2 for both cases.
-model_total  <- glm(death ~ sex, data = df,        family = binomial)
-model_cancer <- glm(death ~ sex, data = df_cancer, family = binomial)
+# linear models to get R^2 for both cases.
+model_total  <- lm(death ~ sex, data = df)
+model_cancer <- lm(death ~ sex, data = df_cancer)
 
-pseudoR2_total  <- 1 - model_total$deviance  / model_total$null.deviance
-pseudoR2_cancer <- 1 - model_cancer$deviance / model_cancer$null.deviance
+R2_total  <- summary(model_total)$r.squared
+R2_cancer <- summary(model_cancer)$r.squared
 
-pseudoR2_total
-pseudoR2_cancer
+R2_total
+R2_cancer
 
 # Table to format results nicely.
 combined_table <- tibble::tibble(
   Analysis        = c("All patients", "Cancer patients"),
   Chi_square_p    = c(chi_total$p.value, chi_cancer$p.value),
-  Pseudo_R2       = c(pseudoR2_total,   pseudoR2_cancer),
+  R2              = c(R2_total,       R2_cancer),
   Male_death_rate = c(
     total_stats$death_rate[total_stats$sex == "male"],
     cancer_stats$death_rate[cancer_stats$sex == "male"]
@@ -97,15 +101,46 @@ combined_table <- tibble::tibble(
 
 combined_table
 
-
 DF_result <- tibble(
   Analysis = c("All patients", "Cancer patients"),
   models   = list(model_total, model_cancer)
-) %>%
-  left_join(combined_table, by = "Analysis") %>%
+) |>
+  left_join(combined_table, by = "Analysis") |>
   mutate(variable = "sex", .before = 1)
 
 DF_result
-#DF_result$models[[1]]
-#DF_result$models[[2]]
+
+DF_result$models[[1]]  # total
+DF_result$models[[2]]  # cancer
+
+
+
+# we wish to create a format that follow the other group members.
+
+# we wish to create a linear model that binds male = 1 and female = 0
+# and then create a linear model of the survival rate
+# for men and female accordingly.
+
+df <- df |>
+  mutate(
+    male_binary = ifelse(sex == "male", 1, 0),
+    survival = 1 - death
+  )
+
+female_model <- lm(survival ~ 1, data = df |> filter(sex == "female"))
+male_model   <- lm(survival ~ 1, data = df |> filter(sex == "male"))
+
+model_df <- tibble(
+  group = c("female", "male"),
+  model = list(female_model, male_model)
+)
+
+model_df
+
+summary(model_df$model[[1]])
+summary(model_df$model[[2]])
+
+
+saveRDS(model_df, here("data", "models", "sex.RDS"))
+readRDS(here("data", "models", "sex.RDS"))
 
