4b2719f5e1a530bd5d22813b877922dad1ecae19
s235350 <s235350@dtu.dk>
2025-12-01 21:06:19 +0100

    Fixed a function

--- R/99_functions.R
+++ R/99_functions.R
@@ -35,4 +35,6 @@ render_to_results <- function(file_name){
     from=here(glue("R/{file_name}_files")),
     to = here(glue("results/{file_name}_files"))
     )
+
+  unlink("R/{file_name}_files", recursive = TRUE)
 }
